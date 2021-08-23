c35cb8398d8452a07f18ab43f5a7644ae69ae857
zzk <479432115@qq.com>
2021-08-23 23:31:04 +0800

    8.23

---
+++ app/src/main/java/com/example/mystudygogo/simple01/BlankFragment1.java
@@ -0,0 +1,46 @@
+package com.example.mystudygogo.simple01;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.mystudygogo.R;
+
+
+public class BlankFragment1 extends Fragment {
+
+    private View root;
+    private TextView textview;
+    private Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (root==null){
+            root = inflater.inflate(R.layout.fragment_blank1,container,false);
+        }
+        textview = root.findViewById(R.id.textview);
+        button  = root.findViewById(R.id.btn);
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               textview.setText("yes, i'm fine. And you ? ");
+           }
+       });
+
+        return root;
+    }
+
+}
---
+++ app/src/main/java/com/example/mystudygogo/simple01/MainActivity1.java
@@ -0,0 +1,16 @@
+package com.example.mystudygogo.simple01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mystudygogo.R;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+}
