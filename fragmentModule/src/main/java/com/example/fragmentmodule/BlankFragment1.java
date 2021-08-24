62cff1dd8fab3638b5a3e0314e80f0bbbfdb76aa
ak <479432115@qq.com>
2021-08-24 18:05:48 +0800

    2021.8.24_add

---
+++ fragmentModule/src/main/java/com/example/fragmentmodule/BlankFragment1.java
@@ -0,0 +1,26 @@
+package com.example.fragmentmodule;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BlankFragment1 extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank1, container, false);
+    }
+}
--- fragmentModule/src/main/java/com/example/fragmentmodule/MainActivity.java
+++ fragmentModule/src/main/java/com/example/fragmentmodule/MainActivity.java
@@ -2,14 +2,50 @@
 package com.example.fragmentmodule;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private Button button1,button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.btn1);
+        button2 = findViewById(R.id.btn2);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn1:
+                replaceFragment(new BlankFragment1());
+                break;
+            case R.id.btn2:
+                replaceFragment(new ItemFragment());
+                break;
+            default:
+                break;
+        }
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction translation = fragmentManager.beginTransaction();
+        translation.replace(R.id.frameLayout,fragment);
+        translation.addToBackStack(null);//添加到栈中
+        translation.commit();
+
     }
 }
