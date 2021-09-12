261794cc0d8e60100f417bb50f51375d1ec0f76a
zzk <479432115@qq.com>
2021-09-13 07:27:00 +0800

    add permission utils

---
+++ bluetoothtest/src/main/java/com/example/bluetoothtest/PermissionHelper.java
@@ -0,0 +1,4 @@
+package com.example.bluetoothtest;
+
+public class PermissionHelper {
+}
---
+++ bluetoothtest/src/main/java/com/example/bluetoothtest/UnitApp.java
@@ -0,0 +1,11 @@
+package com.example.bluetoothtest;
+
+import android.app.Application;
+
+public class UnitApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
